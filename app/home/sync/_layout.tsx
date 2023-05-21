d68dd823bdddfb841a13791d6dfd153ebac911bb
Gavin Kasdorf <me@gkasdorf.com>
2023-05-20 21:36:47 -0800

    fix screen names

--- app/home/sync/_layout.tsx
+++ app/home/sync/_layout.tsx
@@ -11,6 +11,10 @@ const SyncLayout = () => {
                 }}
             />
             <Stack.Screen name={"viewSync"} options={{title: "Sync"}} />
+            <Stack.Screen name={"newSync/stepOne"} options={{title: "New Sync"}} />
+            <Stack.Screen name={"newSync/stepTwo"} options={{title: "New Sync"}} />
+            <Stack.Screen name={"newSync/stepThree"} options={{title: "New Sync"}} />
+            <Stack.Screen name={"newSync/stepFour"} options={{title: "New Sync"}} />
         </Stack>
     );
 };
